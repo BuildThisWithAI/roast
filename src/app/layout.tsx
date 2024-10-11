c401816da1179fc2b8c153b6173609664518b2e1
gneiru <noelrohi59@gmail.com>
2024-10-11 09:48:33 +0800

    fix og details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { siteConfig } from "@/constants";
 import { env } from "@/env.mjs";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,6 +20,29 @@ export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    url: env.NEXT_PUBLIC_APP_URL,
+    siteName: siteConfig.name,
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: siteConfig.name,
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
